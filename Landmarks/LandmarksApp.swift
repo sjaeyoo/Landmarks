76750ae9e72586398f867037a8586d55d62d48da
Seongjae Yoo <sjae.dev@gmail.com>
2024-07-04 13:11:17 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by sjaeyoo on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
