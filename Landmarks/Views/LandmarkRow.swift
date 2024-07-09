1beaef3eb35551bf2ee28c9e05d754088c0fd7d3
Seongjae Yoo <sjae.yoo@gmail.com>
2024-07-09 19:40:58 +0900

    Preview tutorial2. Preview can be customized.

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -16,10 +16,10 @@ struct LandmarkRow: View {
     }
 }
 
-#Preview("Turtle Rock") {
-    LandmarkRow(landmark: landmarks[0])
+#Preview {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
 }
 
-#Preview("Salmon") {
-    LandmarkRow(landmark: landmarks[1])
-}
