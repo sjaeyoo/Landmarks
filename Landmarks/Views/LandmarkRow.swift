d4b16cf91b7a124ec27e3bee365659c408a7ff21
Seongjae Yoo <sjae.yoo@gmail.com>
2024-07-09 19:37:17 +0900

    Simple custom row to display list.

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    var landmark: Landmark
+    
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()    // 크기 조절 가능하게 세팅
+                .frame(width:50, height:50)
+            Text(landmark.name)
+            Spacer()            // 우측 공간 여유분 지정해서 왼쪽 정렬
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
