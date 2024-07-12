8e54d60fa47044c93668f4bd4cdf7d16dad9f25e
Seongjae Yoo <sjae.yoo@gmail.com>
2024-07-12 18:25:08 +0900

    View transition & animation transaction.

--- Landmarks/Views/Hikes/HikeView.swift
+++ Landmarks/Views/Hikes/HikeView.swift
@@ -26,7 +26,10 @@ struct HikeView: View {
                 Spacer()
 
                 Button {
-                    showDetail.toggle()
+                    // Animation 을 transaction 으로 처리. showDetail boolean 값 바뀌고 애니메이션이 나오는 동안 중복 입력을 막음
+                    withAnimation {
+                        showDetail.toggle()
+                    }
                 } label: {
                     Label("Graph", systemImage: "chevron.right.circle")
                         .labelStyle(.iconOnly)
@@ -39,10 +42,24 @@ struct HikeView: View {
 
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade)
             }
         }
     }
 }
+
+
+// AnyTransition 확장으로 moveAndFade 라는 이름의 트랜지션 정의
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        AnyTransition.asymmetric(
+            // 애니메이션 엮기. combined 를 쓰면 동시 동작
+            insertion: .move(edge: .trailing).combined(with: .opacity),
+            removal: .scale.combined(with: .opacity)
+        )
+    }
+}
+
 
 #Preview {
     VStack {
